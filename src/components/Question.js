a477ba2c5ba26bc7a80cbf52e089053ca649c52f
ayman jarmoune <ayman.jarmoune@gmail.com>
2022-11-01 15:12:46 +0100

    changes

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { stateContext } from '../hooks/useStateContext'
 
 export default function Question() {
+   const {context,setContext}= useContext(stateContext)
+   setContext({
+    ...context,
+    timeTaken:1
+   })
   return (
-    <div>Question</div>
+    
   )
 }
